ceecba4bf77b4ff2b822b696f8cc4db9d8d54652
Sachin Jangir <support@ItzSjDude.in>
2020-10-16 09:09:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -60,7 +60,6 @@ apt-get install -y \
  
 pip3 install --upgrade pip setuptools 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && apt install -y ./google-chrome-stable_current_amd64.deb && rm -rf google-chrome-stable_current_amd64.deb
-mkdir /root/ItzSjDude
 git clone https://github.com/ItzSjDude/PikachuUserbot /root/ItzSjDude
 mkdir /root/ItzSjDude/bin/  && mkdir root/ItzSjDude/pikabot/main_plugs/
 cd root/ItzSjDude && pip3 install -r requirements.txt 
